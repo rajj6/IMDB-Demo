54a37d9e13849640178611477299c91e1634fe82
krishnkant jaiswal <krishnkant.1si16ec037@gmail.com>
2021-03-04 15:03:41 +0530

    Adds Producer Entity

--- src/main/java/com/raj/imdbdemo/entity/Producers.java
+++ src/main/java/com/raj/imdbdemo/entity/Producers.java
@@ -1,14 +1,72 @@
-//package com.raj.imdbdemo.entity;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.Table;
-//
-//@Entity
-//@Table(name = "producers")
-//public class Producers {
-//
-//    private String name;
-//    private String sex;
-//    private Integer DOB;
-//    private String bio;
-//}
+package com.raj.imdbdemo.entity;
+
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "producers")
+public class Producers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String sex;
+    private Date DOB;
+
+    @Type(type = "org.hibernate.type.TextType")
+    private String bio;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Date getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(Date DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public Producers(String name, String sex, Date DOB, String bio) {
+        this.name = name;
+        this.sex = sex;
+        this.DOB = DOB;
+        this.bio = bio;
+    }
+
+    public Producers() {
+    }
+}
