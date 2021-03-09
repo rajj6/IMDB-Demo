2f908f8f60df78a0bca3243212e3654871d5d897
krishnkant jaiswal <krishnkant.1si16ec037@gmail.com>
2021-03-09 16:30:31 +0530

    Removes boilerplate code from Movie-Entity by lombok

--- src/main/java/com/raj/imdbdemo/entity/Movie.java
+++ src/main/java/com/raj/imdbdemo/entity/Movie.java
@@ -1,11 +1,16 @@
 package com.raj.imdbdemo.entity;
 
+import lombok.*;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "movies")
 public class Movie {
@@ -30,89 +35,4 @@ public class Movie {
 
     @ManyToMany
     private Set<Actor> actors = new HashSet<>();
-
-    public Movie() {
-    }
-
-    public Movie(String name, Integer yearOfRelease, String plot, byte[] poster, String imgType) {
-        this.name = name;
-        this.yearOfRelease = yearOfRelease;
-        this.plot = plot;
-        this.poster = poster;
-        this.imgType = imgType;
-    }
-
-    public byte[] getPoster() {
-        return poster;
-    }
-
-    public void setPoster(byte[] poster) {
-        this.poster = poster;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getYearOfRelease() {
-        return yearOfRelease;
-    }
-
-    public void setYearOfRelease(Integer yearOfRelease) {
-        this.yearOfRelease = yearOfRelease;
-    }
-
-    public String getPlot() {
-        return plot;
-    }
-
-    public void setPlot(String plot) {
-        this.plot = plot;
-    }
-
-    public String getImgType() {
-        return imgType;
-    }
-
-    public void setImgType(String imgType) {
-        this.imgType = imgType;
-    }
-
-    public Producer getProducer() {
-        return producer;
-    }
-
-    public void setProducer(Producer producer) {
-        this.producer = producer;
-    }
-
-    public Set<Actor> getActors() {
-        return actors;
-    }
-
-    public void setActors(Set<Actor> actors) {
-        this.actors = actors;
-    }
-
-    @Override
-    public String toString() {
-        return "Movie{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", yearOfRelease=" + yearOfRelease +
-                ", plot='" + plot + '\'' +
-                '}';
-    }
 }
