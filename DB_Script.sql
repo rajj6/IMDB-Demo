3bd2e73f0903e3d85ff31e06194af0d230ac0763
krishnkant jaiswal <krishnkant.1si16ec037@gmail.com>
2021-03-09 18:52:16 +0530

    Adds  database script(DB_Script.sql) file

---
+++ DB_Script.sql
@@ -0,0 +1,48 @@
+drop table if exists movies_actors cascade;
+drop table if exists actors cascade;
+drop table if exists movies cascade;
+drop table if exists producers cascade;
+
+create table actors (
+       id int8 not null,
+        dob date,
+        bio text,
+        name varchar(255),
+        sex varchar(255),
+        primary key (id)
+    );
+	
+create table producers (
+       id int8 not null,
+        dob date,
+        bio text,
+        name varchar(255),
+        sex varchar(255),
+        primary key (id)
+    );
+	
+create table movies (
+       id int8 not null,
+        img_type varchar(255),
+        name varchar(255),
+        plot text,
+        poster bytea,
+        year_of_release int4,
+        producer_id int8,
+        primary key (id),
+		CONSTRAINT fk_producer
+      		FOREIGN KEY(producer_id) 
+	  			REFERENCES producers(id)
+    );
+    
+create table movies_actors (
+       movies_id int8 not null,
+       actors_id int8 not null,
+       primary key (movies_id, actors_id),
+	 CONSTRAINT fk_movie
+      		FOREIGN KEY(movies_id) 
+	  			REFERENCES movies(id),
+	CONSTRAINT fk_actor
+      		FOREIGN KEY(actors_id) 
+	  			REFERENCES actors(id)
+    );
