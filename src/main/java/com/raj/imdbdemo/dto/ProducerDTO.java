e1ca1451e6272f494f19abd4513b5ccb91c8c912
krishnkant jaiswal <krishnkant.1si16ec037@gmail.com>
2021-03-09 07:51:42 +0530

    Reduces boilerplate codes from ProducerDTO using lombok

--- src/main/java/com/raj/imdbdemo/dto/ProducerDTO.java
+++ src/main/java/com/raj/imdbdemo/dto/ProducerDTO.java
@@ -1,79 +1,21 @@
 package com.raj.imdbdemo.dto;
 
 import com.raj.imdbdemo.entity.Producer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
 public class ProducerDTO {
     private Long id;
     private String name;
     private String sex;
     private Date DOB;   //DateOfBirth
     private String bio;
-
-    @Override
-    public String toString() {
-        return "ProducerDTO{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", sex='" + sex + '\'' +
-                ", DOB=" + DOB +
-                ", bio='" + bio + '\'' +
-                '}';
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public Date getDOB() {
-        return DOB;
-    }
-
-    public void setDOB(Date DOB) {
-        this.DOB = DOB;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    public ProducerDTO(Long id, String name, String sex, Date DOB, String bio) {
-        this.id = id;
-        this.name = name;
-        this.sex = sex;
-        this.DOB = DOB;
-        this.bio = bio;
-    }
-
-    public ProducerDTO() {
-    }
 
     public static ProducerDTO mapEntityToDTO(Producer producer) {
         return new ProducerDTO(producer.getId(), producer.getName(), producer.getSex(), producer.getDOB(), producer.getBio());
